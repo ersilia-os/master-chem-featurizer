d87eef0b5eb9c53cf9c27122683e40a6725d7697
miquelduranfrigola <miquelduranfrigola@gmail.com>
2021-11-21 16:39:37 +0200

    refactor library

--- molbertfeat/library.py
+++ molbertfeat/library.py
@@ -16,21 +16,22 @@ from tqdm import tqdm
 
 
 class ReferenceLibrary(object):
-    def __init__(self, file_name=None, max_molecules=100000000, chunksize=1000):
+    def __init__(self, file_name=None, max_molecules=100000000, chunksize=1000, write_chunksize=10000, standarise=False):
+        assert chunksize <= write_chunksize
         if file_name is None:
             self.file_name = REFERENCE_SMILES
         else:
             self.file_name = file_name
         self.max_molecules = max_molecules
         self.chunksize = chunksize
+        self.standardise = standardise
 
     def _read_file_only_valid(self):
         smiles_list = []
         with open(self.file_name, "r") as f:
-            reader = csv.reader(f, delimiter="\t")
-            next(reader)
+            reader = csv.reader(f)
             for i, r in enumerate(reader):
-                smiles_list += [r[1]]
+                smiles_list += [r[0]]
         smiles_list = smiles_list[:self.max_molecules]
         f = self.mdl.model.featurizer
         std_smiles = []
@@ -45,7 +46,16 @@ class ReferenceLibrary(object):
             valid_smiles = f.is_legal(standard_smiles) and f.is_short(decorated_smiles)
             if valid_smiles:
                 val_smiles += [standard_smiles]
+        val_smiles = list(set(val_smiles)) # Unique
         return val_smiles
+
+    def _read_file_assuming_valid(self):
+        smiles_list = []
+        with open(self.file_name, "r") as f:
+            reader = csv.reader(f)
+            for i, r in enumerate(reader):
+                smiles_list += [r[0]]
+        return smiles_list
 
     def save_only_smiles(self, csv_file):
         self.mdl = Featurizer(standardise=True)
@@ -55,15 +65,59 @@ class ReferenceLibrary(object):
             for smi in smiles_list:
                 writer.writerow([smi])
 
-    def save(self, h5_file):
-        assert h5py is not None
-        self.mdl = Featurizer(standardise=True, chunksize=self.chunksize)
-        smiles_list = self._read_file_only_valid()
-        X = self.mdl.transform(smiles_list)
-        with h5py.File(h5_file, "w") as f:
-            f.create_dataset("Values", data=X)
+    def _get_already_done_inputs(self, h5_file):
+        with h5py.File(h5_file, "r") as f:
+            inps = f["Inputs"][:]
+            inps = set([x.decode("utf-8") for x in inps])
+        return inps
+
+    def _get_todo_smiles(self, smiles_list, h5_file):
+        done_smiles = self._get_already_done_inputs(h5_file)
+        todo_smiles = []
+        for smi in smiles_list:
+            if smi in done_smiles:
+                todo_smiles += [smi]
+        return todo_smiles
+
+    def chunked_iterable(self, seq):
+        return (seq[pos:pos + self.chunksize] for pos in range(0, len(seq), self.write_chunksize))
+
+    def append_to_h5(self, h5_file, X, smiles_list):
+        with open(h5_file, "a") as f:
             smiles_list = np.array(smiles_list, h5py.string_dtype())
+            f["Values"].resize(f["Values"].shape[0] + X.shape[0], axis=0)
+            f["Values"][-X.shape[0]:] = X
+            f["Inputs"].resize(f["Inputs"].shape[0] + smiles_list.shape[0], axis=0)
+            f["Inputs"][-smiles_list.shape[0]:] = smiles_list
+
+    def write_to_h5(self, h5_file, X, smiles_list):
+        with open(h5_file, "w") as f:
+            smiles_list = np.array(smiles_list, h5py.string_dtype())
+            f.create_dataset("Values", data=X)
             f.create_dataset("Inputs", data=smiles_list)
+
+    def save(self, h5_file, append=True):
+        assert h5py is not None
+        self.mdl = Featurizer(standardise=self.standarise, chunksize=self.chunksize)
+        if self.standardise:
+            smiles_list = self._read_file_only_valid()
+        else:
+            smiles_list = self._read_file_assuming_valid()
+        file_exists = os.path.isfile(h5_file)
+        if file_exists and append:
+            smiles_list = self._get_todo_smiles(smiles_list, h5_file)
+            for chunk in self.chunked_iterable(smiles_list):
+                X = self.mdl.transform(chunk)
+                self.append_to_h5(h5_file, X, chunk)
+        else:
+            if file_exists:
+                os.remove(h5_file)
+            for i, chunk in self.chunked_iterable(smiles_list):
+                X = self.mdl.transform(chunk)
+                if i == 0:
+                    self.write_to_h5(h5_file, X, chunk)
+                else:
+                    self.append_to_h5(h5_file, X, chunk)
 
     def read(self, h5_file):
         assert h5py is not None
